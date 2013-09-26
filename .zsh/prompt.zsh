653941aae16eb4890230ded0103674c5855e6089
Nicholas Firth-McCoy <nicholas@2suggestions.com.au>
2013-09-26 16:04:58 +1000

    Tweak zsh prompt

--- .zsh/prompt.zsh
+++ .zsh/prompt.zsh
@@ -3,12 +3,12 @@ git_branch() {
 }
 
 git_dirty() {
-  STATUS=$(git status -s 2> /dev/null | tail -n 1)
-  if [[ -n ${STATUS} ]]
+  STATUS=$(git status -s 2> /dev/null | wc -l)
+  if [[ ${STATUS} -eq 0 ]]
   then
-    echo "%{$fg[green]%}!%{$reset_color%}"
+    echo "green"
   else
-    echo ""
+    echo "red"
   fi
 }
 
@@ -16,18 +16,23 @@ git_prompt_info () {
   BRANCH=$(git_branch)
   if [[ ${BRANCH} != "" ]]
   then
-    echo "%{$fg[green]%}${BRANCH}$(git_dirty)$(need_push)%{$reset_color%} "
+    echo "%{$fg[$(git_dirty)]%}${BRANCH}$(need_push)%{$reset_color%} "
   fi
 }
 
 need_push () {
-  UNPUSHED=$(git cherry -v origin/$(git_branch) 2>/dev/null)
-  if [[ -n ${UNPUSHED} ]]
+  UNPUSHED=$(git cherry -v origin/$(git_branch) 2>/dev/null | wc -l)
+  if [[ ${UNPUSHED} -gt 0 ]]
   then
-    echo "%{$fg[green]%}›%{$reset_color%}"
+    if [[ ${UNPUSHED} -gt 3 ]]
+    then
+      echo " ${UNPUSHED}›"
+    else
+      echo "$(repeat ${UNPUSHED} printf "›";print)"
+    fi
   fi
 }
 
 precmd () {
-  PROMPT="%{$fg[blue]%}%m:%{$reset_color%} %{$fg[yellow]%}%~ %{$reset_color%}%{$fg[cyan]%}% %{$reset_color%}$(git_prompt_info)$ "
+  PROMPT="%{$fg[blue]%}%m%{$reset_color%} %{$fg[yellow]%}%~ %{$reset_color%}$(git_prompt_info)%{$fg[yellow]%}$%{$reset_color%} "
 }
