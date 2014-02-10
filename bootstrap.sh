d1cf596335f06d8b33f15b50a112f11f873b9523
Nicholas Firth-McCoy <nicholas@2suggestions.com.au>
2014-02-11 09:34:16 +1100

    Ensure directories are preserved

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env bash
 
-cd "$(dirname "${0}")"
+SCRIPT_DIR=`dirname "${0}"`
+SCRIPT_FILE=`basename "${0}"`
+
+cd "$SCRIPT_DIR"
 
 read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1
 echo
 
 if [[ $REPLY =~ ^[Yy]$ ]]; then
-  rsync -a --no-perms --out-format "%n%L" `git ls-files` ~
+  rsync -a --no-perms --out-format "%n%L" --exclude .git --exclude "$SCRIPT_FILE" . ~/
 fi
